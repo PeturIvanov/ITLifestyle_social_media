b3a743437fbdc55c5bbd36806d7b221a35a4e0b5
Petur <peturivanov4712@gmail.com>
2023-09-08 17:02:23 +0300

    profiles page, home, basic setting and models

--- itlifestyle/views.py
+++ itlifestyle/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.shortcuts import render, redirect
+from .models import Profile
 
-# Create your views here.
+
+# Create Home View
+def home(request):
+    return render(request, 'home.html', {})
+
+
+# Create All profiles view
+def profile_list(request):
+    if request.user.is_authenticated:
+        profiles = Profile.objects.exclude(user=request.user)
+        return render(request, 'profile_list.html', {'profiles': profiles})
+
+    else:
+        messages.success(request, "You Must Be Logged In To View This Page!")
+        return redirect('home')
+
+
